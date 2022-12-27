d0144d3db3b6c6dd3ed854a9fbf581fab36f478e
MartinBoychev <boycheff88@gmail.com>
2022-12-27 09:18:02 +0200

    New task

--- syntax_basics/nums1_100.py
+++ syntax_basics/nums1_100.py
@@ -2,5 +2,5 @@ number = float(input())
 
 while number < 1 or number > 100:
     number = float(input())
-else:
-    print(f"The number {number} is between 1 and 100")
+
+print(f"The number {number} is between 1 and 100")
