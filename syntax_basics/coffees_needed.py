1cc9b1b52bd5ad1863a27b20b3c07f3a12c605e2
MartinBoychev <boycheff88@gmail.com>
2022-12-29 22:41:05 +0200

    New task

---
+++ syntax_basics/coffees_needed.py
@@ -0,0 +1,12 @@
+action = input()
+coffees = 0
+while action != "END":
+    if action == "CODING" or action == "DOG" or action == "MOVIE" or action == "CAT":
+        coffees += 2
+    elif action == "coding" or action == "dog" or action == "movie" or action == "cat":
+        coffees += 1
+    action = input()
+if coffees > 5:
+    print("You need extra sleep")
+else:
+    print(coffees)
--- syntax_basics/double_char.py
+++ syntax_basics/double_char.py
@@ -4,5 +4,5 @@ while string != "End":
     if string == "SoftUni":
         string = input()
     else:
-        print('S'.join(c + c for c in string))
+        print(''.join(c + c for c in string))
         string = input()
