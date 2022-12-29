d66d95f97f5536d7bd42bf1acfed52cdcf899a69
MartinBoychev <boycheff88@gmail.com>
2022-12-29 21:12:26 +0200

    New task

---
+++ syntax_basics/orders.py
@@ -0,0 +1,14 @@
+n = int(input())
+total_money = 0
+
+for _ in range(n):
+    price_capsule = float(input())
+    days = int(input())
+    capsules_per_day = int(input())
+    total_price = price_capsule * days * capsules_per_day
+    if 0.01 <= price_capsule <= 100.00 and 1 <= days <= 31 and 1 <= capsules_per_day <= 2000:
+        print(f"The price for the coffee is: ${total_price:.2f}")
+        total_money += total_price
+    else:
+        pass
+print(f"Total: ${total_money:.2f}")
