d1e30ee40d810c62b5c62d1ad7ef17ea517b8287
MartinBoychev <boycheff88@gmail.com>
2022-12-31 20:14:26 +0200

    New task

--- syntax_basics/easter_bread.py
+++ syntax_basics/easter_bread.py
@@ -7,12 +7,16 @@ money_per_bread = price_floor + eggs_price + milk_price
 money_left = budget - money_per_bread
 current_bread_count = 0
 colored_eggs = 0
-while budget < money_per_bread:
+
+while money_left > 0:
     money_left -= money_per_bread
     current_bread_count += 1
     colored_eggs += 3
     if current_bread_count % 3 == 0:
-        colored_eggs = current_bread_count - 2
+        colored_eggs = colored_eggs - (current_bread_count - 2)
+    continue
 
-print(f"You made {current_bread_count} loaves of Easter bread! Now you have {colored_eggs} eggs and {money_left}BGN left.")
+final_money = budget - (current_bread_count * money_per_bread)
+
+print(f"You made {current_bread_count} loaves of Easter bread! Now you have {colored_eggs} eggs and {final_money:.2f}BGN left.")
 
