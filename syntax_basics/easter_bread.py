428e36edc8fa54067fb63073fa69bef11515e567
MartinBoychev <boycheff88@gmail.com>
2022-12-31 19:48:01 +0200

    New task

--- syntax_basics/easter_bread.py
+++ syntax_basics/easter_bread.py
@@ -2,5 +2,17 @@ budget = float(input())
 price_floor = float(input())
 
 eggs_price = price_floor * 0.75
-milk_price = price_floor * 1.25
+milk_price = (price_floor * 1.25) * 0.25
+money_per_bread = price_floor + eggs_price + milk_price
+money_left = budget - money_per_bread
+current_bread_count = 0
+colored_eggs = 0
+while budget < money_per_bread:
+    money_left -= money_per_bread
+    current_bread_count += 1
+    colored_eggs += 3
+    if current_bread_count % 3 == 0:
+        colored_eggs = current_bread_count - 2
+
+print(f"You made {current_bread_count} loaves of Easter bread! Now you have {colored_eggs} eggs and {money_left}BGN left.")
 
