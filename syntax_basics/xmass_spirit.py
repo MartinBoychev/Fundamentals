f7ff2b1cb7e9f70ee068887b942f2e545e7fb5e3
MartinBoychev <boycheff88@gmail.com>
2023-01-02 00:01:30 +0200

    New task

--- syntax_basics/xmass_spirit.py
+++ syntax_basics/xmass_spirit.py
@@ -1,33 +1,30 @@
 decor_qty = int(input())
 day = int(input())
 
-days_till_xmas = day
 total_buying = 0
 points = 0
-while days_till_xmas > 1:
-    days_till_xmas -= 1
-    if days_till_xmas % 2 == 0:
+count_days = 0
+while day > 0:
+    count_days += 1
+    day -= 1
+    if count_days % 11 == 0:
+        decor_qty += 2
+    if count_days % 2 == 0:
         total_buying += decor_qty * 2
         points += 5
-    if days_till_xmas % 3 == 0:
+    if count_days % 3 == 0:
         total_buying += decor_qty * 5 + decor_qty * 3
         points += 13
-    if days_till_xmas % 5 == 0:
+    if count_days % 5 == 0:
         total_buying += decor_qty * 15
         points += 17
-    if days_till_xmas % 10 == 0:
+    if count_days % 15 == 0:
+        points += 30
+    if count_days % 10 == 0:
+        total_buying += 23
         points -= 20
-        total_buying += decor_qty * 2 + decor_qty * 5 + decor_qty * 3 + decor_qty * 15
-    if days_till_xmas % 11 == 0:
-        decor_qty += 2
-    if days_till_xmas == 10:
+    if count_days % 10 == 0 and day == 0:
         points -= 30
+
 print(f"Total cost: {total_buying}")
-print(f"Total spirit {points}")
-
-
-
-
-
-
-
+print(f"Total spirit: {points}")
