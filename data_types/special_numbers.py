f7f2594f82da97476ed5f37767c659b698aa4caf
MartinBoychev <boycheff88@gmail.com>
2023-01-06 00:00:37 +0200

    New task

--- data_types/special_numbers.py
+++ data_types/special_numbers.py
@@ -6,9 +6,12 @@ n = int(input())
 
 for i in range(1, n + 1):
     sum_digits = 0
-    for digit in str(i):
-        sum_digits += int(digit)
+    digit = i
+    while digit > 0:
+        sum_digits += digit % 10
+        digit = int(digit / 10)
     if sum_digits == 5 or sum_digits == 7 or sum_digits == 11:
         print(f"{i} -> True")
     else:
         print(f"{i} -> False")
+
