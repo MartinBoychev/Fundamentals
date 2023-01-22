9631710bd85b9ab7452ad00fdb95b7d9343953a2
Boycheff <76918675+MartinBoychev@users.noreply.github.com>
2023-01-22 12:11:03 +0200

    Update snowballs.py

--- data_types/snowballs.py
+++ data_types/snowballs.py
@@ -2,19 +2,18 @@ num_snowballs = int(input())
 snowball_weight_highest = 0
 snowball_time_highest = 0
 snowball_quality_highest = 0
-value = 0
+best_value = 0
+
 for _ in range(num_snowballs):
     snowball_weight = int(input())
-    if snowball_weight > snowball_weight_highest:
-        snowball_weight_highest = snowball_weight
     snowball_time = int(input())
-    if snowball_time > snowball_time_highest:
-        snowball_time_highest = snowball_time
     snowball_quality = int(input())
-    if snowball_quality > snowball_quality_highest:
+    value = (snowball_weight / snowball_time) ** snowball_quality
+
+    if value > best_value:
+        best_value = int(value)
+        snowball_weight_highest = snowball_weight
+        snowball_time_highest = snowball_time
         snowball_quality_highest = snowball_quality
 
-value = (snowball_weight_highest / snowball_time_highest) ** snowball_quality_highest
-print(f"{snowball_weight_highest} : {snowball_time_highest} = {value} ({snowball_quality_highest})")
-
-
+print(f"{snowball_weight_highest} : {snowball_time_highest} = {best_value} ({snowball_quality_highest})")
