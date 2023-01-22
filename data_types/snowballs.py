6d8030cf04c542134f628cbb81735a827bd80977
Boycheff <76918675+MartinBoychev@users.noreply.github.com>
2023-01-22 12:22:51 +0200

    task updated

--- data_types/snowballs.py
+++ data_types/snowballs.py
@@ -1,3 +1,17 @@
+# Tony and Andi love playing in the snow and having snowball fights,
+# but they always argue about who makes the best snowballs.
+# They have decided to involve you in their fray by writing a program
+# that calculates snowball data and outputs the best snowball value.
+# You will receive N – an integer, the number of snowballs being made by Tony and Andi.
+# On the following lines, you will receive 3 inputs for each snowball:
+# ⦁	The weight of the snowball (integer).
+# ⦁	The time needed for the snowball to get to its target (integer).
+# ⦁	The quality of the snowball (integer).
+# For each snowball, you must calculate its value by the following formula:
+# (snowball_weight / snowball_time) ** snowball_quality
+# In the end, you must print the highest calculated value of a snowball.
+
+
 num_snowballs = int(input())
 snowball_weight_highest = 0
 snowball_time_highest = 0
