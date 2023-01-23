ce94df6d5d5bcb6d361e4045737fd085bc393188
Boycheff <76918675+MartinBoychev@users.noreply.github.com>
2023-01-23 18:46:03 +0200

    update invert_values.py

--- lists/invert_values.py
+++ lists/invert_values.py
@@ -1,4 +1,12 @@
-list_input = input()
-my_list = list(list_input)
+# Write a program that receives a single string containing positive and
+# negative numbers separated by a single space.
+# Print a list containing the opposite of each number.
 
-print(reversed[my_list])
+my_list = input().split()
+opposite_list = []
+
+for i in range(len(my_list)):
+    opposite_number = -int(my_list[i])
+    opposite_list.append(opposite_number)
+
+print(opposite_list)
