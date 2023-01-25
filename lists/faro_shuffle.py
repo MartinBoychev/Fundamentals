8bde896bb5630006f09aadb5242a54d9bb565817
Boycheff <76918675+MartinBoychev@users.noreply.github.com>
2023-01-25 22:56:07 +0200

    Update faro_shuffle.py

--- lists/faro_shuffle.py
+++ lists/faro_shuffle.py
@@ -1,9 +1,9 @@
 # A faro shuffle is a method for shuffling a deck of cards, in which the deck is split exactly in half. Then the
 # cards in the two halves are perfectly interleaved, such that the original bottom card is still on the bottom and
 # the original top card is still on top. For example, faro shuffling the list ['ace', 'two', 'three', 'four', 'five',
-# 'six'] once, gives ['ace', 'four', 'two', 'five', 'three', 'six'] Write a program that receives a single string (
-# cards separated by space) and on the second line receives a count of faro shuffles that should be made. Print the
-# state of the deck after the shuffle. Note: The length of the deck of cards will always be an even number.
+# 'six'] once, gives ['ace', 'four', 'two', 'five', 'three', 'six'] Write a program that receives a single string
+# (cards separated by space) and on the second line receives a count of faro shuffles that should be made.
+# Print the state of the deck after the shuffle. Note: The length of the deck of cards will always be an even number.
 
 cards = input().split()
 shuffles = int(input())
