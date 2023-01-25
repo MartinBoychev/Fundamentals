a6f969a3bcb8489a960ecfd5aa995eb301f5d1a4
Boycheff <76918675+MartinBoychev@users.noreply.github.com>
2023-01-25 21:43:25 +0200

    update mutate_strings.py

--- syntax_basics/mutate_strings.py
+++ syntax_basics/mutate_strings.py
@@ -1,3 +1,8 @@
+# You will be given two strings. Transform the first string into the second one,
+# letter by letter, starting from the first one. After each interaction,
+# print the resulting string only if it is unique.
+# Note: the strings will have the same length.
+
 word = input()
 second_word = input()
 new_string = ""
