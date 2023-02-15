5fb153f7479e04134e7812796812601ea449dcfd
Boycheff <76918675+MartinBoychev@users.noreply.github.com>
2023-02-15 20:32:45 +0200

    New Task Functions

---
+++ mid_exam/mu_online.py
@@ -0,0 +1,36 @@
+rooms = input().split("|")
+
+MAX_HEALTH = 100
+health = MAX_HEALTH
+bitcoins = 0
+
+room_number = 0
+
+for room in rooms:
+    room_number += 1
+    command, amount = room.split()
+    amount = int(amount)
+
+    if command == "potion":
+        if health + amount > MAX_HEALTH:
+            print(f"You healed for {MAX_HEALTH - health} hp.")
+            health = MAX_HEALTH
+        else:
+            print(f"You healed for {amount} hp.")
+            health += amount
+        print(f"Current health: {health} hp.")
+    elif command == "chest":
+        bitcoins += amount
+        print(f"You found {amount} bitcoins.")
+    else:
+        health -= amount
+        if health > 0:
+            print(f"You slayed {command}.")
+        else:
+            print(f"You died! Killed by {command}.")
+            print(f"Best room: {room_number}")
+            exit(0)
+
+print("You've made it!")
+print(f"Bitcoins: {bitcoins}")
+print(f"Health: {health}")
