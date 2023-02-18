a3e55fca656583fb466ed430f83db15abe140ec1
Boycheff <76918675+MartinBoychev@users.noreply.github.com>
2023-02-18 11:42:00 +0200

    New Task Lists

--- list_advanced/group_of_10s.py
+++ list_advanced/group_of_10s.py
@@ -1,30 +1,16 @@
-def sort_into_groups(numbers):
-    # Convert the string of numbers into a list of integers
-    numbers_list = [int(num) for num in numbers.split(", ")]
-
-    # Initialize variables
-    groups = []
-    group_boundary = 10
-
-    # Loop until the list of numbers is empty
-    while numbers_list:
-        # Filter the numbers that belong to the current group
-        current_group = [num for num in numbers_list if num <= group_boundary]
-
-        # Remove the current group from the list of numbers
-        for num in current_group:
-            numbers_list.remove(num)
-
-        # If there are numbers in the current group, add it to the list of groups
-        if current_group:
-            groups.append(current_group)
-
-        # Increase the group boundary by 10
-        group_boundary += 10
-
-    # Print the groups of numbers in the desired format
-    for i, group in enumerate(groups):
-        print(f"Group of {10 * (i + 1)}'s: {group}")
+# Write a program that receives a sequence of numbers (a string containing integers separated by ", ")
+# and prints the numbers sorted into lists of 10's in the format "Group of {group}'s: {list_of_numbers}".
+# Examples:
+# The numbers 2, 8, 4, and 10 fall into the group of 10's.
+# The numbers 13, 19, 14, and 15 fall into the group of 20's.
+# For more clarification, see the examples below.
 
 
-sort_into_groups(input())
+numbers = [int(i) for i in input().split(", ")]
+groups = 0
+
+while numbers:
+    groups += 10
+    print_list = [num for num in numbers if num <= groups]
+    numbers = [num for num in numbers if num > groups]
+    print(f"Group of {groups}'s: {print_list}")
