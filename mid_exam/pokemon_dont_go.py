ad089218ba93d40663f545f4ac1df0f784f8ccd6
Boycheff <76918675+MartinBoychev@users.noreply.github.com>
2023-02-18 21:20:27 +0200

    New Task exam

---
+++ mid_exam/pokemon_dont_go.py
@@ -0,0 +1,40 @@
+# Ely likes to play Pokemon Go a lot. But Pokemon Go bankrupted… So the developers made Pokemon Don't Go out of
+# depression. And so Ely now plays Pokemon Don't Go. In Pokemon Don't Go, when you walk to a certain pokemon,
+# those closest to you naturally get further, and those further from you, get closer. You will receive a sequence of
+# integers, separated by spaces - the distances to the pokemon. Then you will begin receiving integers, which will
+# correspond to indexes in that sequence. When you receive an index, you must remove the element at that index from
+# the sequence (as if you've captured the pokemon). You must increase the value of all elements in the sequence which
+# are less or equal to the removed element with the value of the removed element. You must decrease the value of all
+# elements in the sequence which are greater than the removed element with the value of the removed element. If the
+# given index is less than 0, remove the first element of the sequence, and copy the last element to its place. If
+# the given index is greater than the last index of the sequence, remove the last element from the sequence,
+# and copy the first element to its place. The increasing and decreasing elements should also be done in these cases.
+# The element whose value you should use is the removed element. The program ends when the sequence has no elements (
+# there are no pokemon left for Ely to catch). Input On the first line of input, you will receive a sequence of
+# integers, separated by spaces. On the next several lines, you will receive integers - the indexes. Output When the
+# program ends, you must print the summed value of all removed elements.
+
+sequence = [int(el) for el in input().split()]
+points = 0
+
+while len(sequence) != 0:
+    index = int(input())
+    num = 0
+    if 0 <= index < len(sequence):
+        num = sequence.pop(index)
+    elif 0 > index:
+        num_to_add = sequence[-1]
+        num = sequence[0]
+        sequence[0] = sequence[-1]
+    else:
+        num_to_add = sequence[0]
+        num = sequence[-1]
+        sequence[-1] = sequence[0]
+    points += num
+    for current_index, current_num in enumerate(sequence):
+        if current_num <= num:
+            sequence[current_index] += num
+        else:
+            sequence[current_index] -= num
+
+print(points)
